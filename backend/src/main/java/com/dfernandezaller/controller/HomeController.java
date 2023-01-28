fd8a2a253fcd1bcf8195bc472f845e5ad2c763db
Daniel Fernandez Aller <uo257977@uniovi.es>
2023-01-28 18:01:43 +0100

    Enable security to access backend endpoints based on google token.

--- backend/src/main/java/com/dfernandezaller/controller/HomeController.java
+++ backend/src/main/java/com/dfernandezaller/controller/HomeController.java
@@ -1,22 +1,23 @@
 package com.dfernandezaller.controller;
 
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authentication;
 import io.micronaut.security.rules.SecurityRule;
-import io.micronaut.views.View;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Secured(SecurityRule.IS_AUTHENTICATED)
 public class HomeController {
 
-    @Secured(SecurityRule.IS_ANONYMOUS)
-    @View("home")
+    @Produces(MediaType.APPLICATION_JSON)
     @Get
-    public Map<String, Object> index() {
-        return new HashMap<>();
+    public Map<String, Object> index(Authentication principal) {
+        return Map.of("Testing", "this out", "User:", principal.getName());
     }
 
 }
