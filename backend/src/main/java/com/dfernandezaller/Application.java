f5e7be804b95d97e67d6c99e8c0d151cad17bc1d
Daniel Fernandez Aller <uo257977@uniovi.es>
2023-02-20 20:27:10 +0100

    Generated OpenAPI

--- backend/src/main/java/com/dfernandezaller/Application.java
+++ backend/src/main/java/com/dfernandezaller/Application.java
@@ -11,9 +11,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 @OpenAPIDefinition(
     info = @Info(
             title = "nomoremeetings-backend",
-            version = "0.0"
+            version = "0.0",
+            description = "Backend for NoMoreMeetings"
     )
 )
+// TODO: 20/02/2023 Improve documentation of controllers and endpoints to generate better OpenAPI documentation
 public class Application {
 
     @ContextConfigurer
